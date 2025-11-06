594d84559b6979744974a7d63e1a9200ed395644
Juan Ignacio Cisternas Ramos <72153826+jcisternasr@users.noreply.github.com>
2025-11-06 10:39:19 -0300

    optimizar código procesamiento_general

--- procesamiento_general.R
+++ procesamiento_general.R
@@ -2,81 +2,74 @@
 
 pacman::p_load(
   tidyverse,
-  dplyr, 
-  here,       
-  janitor,    
-  lubridate,  
+  dplyr,
+  here,
+  janitor,
+  lubridate,
   summarytools,
   rio,
   ggthemes,
   ggsci,
   readxl,
   ggplot2,
-  scales)
+  scales
+)
+
+# Función de limpieza reutilizable
+limpiar_nombres_centro <- function(df, col_name = NULL) {
+  df %>%
+    mutate(
+      across(where(is.character), ~ str_replace_all(., regex("Centro de Salud Familiar", ignore_case = TRUE), "CESFAM")),
+      across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
+      across(where(is.character), ~ str_replace_all(., regex("Cerrillos De Nos", ignore_case = TRUE), "Ribera del Maipo")),
+      across(where(is.character), ~ str_replace_all(., regex("Posta de Salud Rural", ignore_case = TRUE), "PSR")),
+      across(where(is.character), ~ str_replace_all(., regex("Cecosf", ignore_case = TRUE), "CECOSF"))
+    )
+}
 
 ####################### Importo los datos ########################################
 
-usuarios <- import('data/Usuarios.xlsx') %>%  clean_names()
+usuarios <- import('data/Usuarios.xlsx') %>% clean_names()
 data <- import('data/base_julio_2025.csv') %>% clean_names()
 piv2024 <- import('data/piv2024.xlsx') %>% clean_names()
 piv2025 <- import('data/piv2025.xlsx') %>% clean_names()
-deis <- import('data/deis_ssms.xlsx') %>% clean_names() %>% rename(centro = nombre_oficial,comuna = nombre_comuna, codigo_centro = codigo_vigente)
+# Se renombra para el join
+deis <- import('data/deis_ssms.xlsx') %>%
+  clean_names() %>%
+  rename(centro = nombre_oficial, comuna = nombre_comuna, codigo_centro = codigo_vigente)
 
-export(head(data),'data_enero_head.csv')
+export(head(data), 'data_enero_head.csv')
 
 ####################### Manejo de la base de PIV 2024 y 2025 ##################################
+
 piv_comuna_2025 <- piv2025 %>%
+  limpiar_nombres_centro() %>%
   group_by(comuna) %>%
   summarize(piv_2025 = sum(inscritos)) %>%
-  ungroup() %>%
-  mutate(across(where(is.character), ~ str_replace_all(., regex("Centro de Salud Familiar", ignore_case = TRUE), "CESFAM")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Cerrillos De Nos", ignore_case = TRUE), "Ribera del Maipo")),
-         across(where(is.character), ~ str_replace_all(., regex("Posta de Salud Rural", ignore_case = TRUE), "PSR")),
-         across(where(is.character), ~ str_replace_all(., regex("Cecosf", ignore_case = TRUE), "CECOSF"))
-  )
+  ungroup()
+
 piv_comuna_2024 <- piv2024 %>%
+  limpiar_nombres_centro() %>%
   group_by(comuna) %>%
   summarize(piv_2024 = sum(inscritos)) %>%
-  ungroup() %>%
-  mutate(across(where(is.character), ~ str_replace_all(., regex("Centro de Salud Familiar", ignore_case = TRUE), "CESFAM")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Cerrillos De Nos", ignore_case = TRUE), "Ribera del Maipo")),
-         across(where(is.character), ~ str_replace_all(., regex("Posta de Salud Rural", ignore_case = TRUE), "PSR")),
-         across(where(is.character), ~ str_replace_all(., regex("Cecosf", ignore_case = TRUE), "CECOSF"))
-  )
+  ungroup()
 
 piv_estab_2025 <- piv2025 %>%
-  clean_names() %>% 
+  clean_names() %>%
+  limpiar_nombres_centro() %>%
   group_by(comuna, centro, codigo_centro) %>%
   summarise(piv_2025 = sum(inscritos)) %>%
-  ungroup() %>%
-  mutate(across(where(is.character), ~ str_replace_all(., regex("Centro de Salud Familiar", ignore_case = TRUE), "CESFAM")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Cerrillos De Nos", ignore_case = TRUE), "Ribera del Maipo")),
-         across(where(is.character), ~ str_replace_all(., regex("Posta de Salud Rural", ignore_case = TRUE), "PSR")),
-         across(where(is.character), ~ str_replace_all(., regex("Cecosf", ignore_case = TRUE), "CECOSF"))
-  )
+  ungroup()
 
 piv_estab_2024 <- piv2024 %>%
-  clean_names() %>% 
+  clean_names() %>%
+  limpiar_nombres_centro() %>%
   group_by(comuna, centro, codigo_centro) %>%
   summarise(piv_2024 = sum(inscritos)) %>%
-  ungroup() %>%
-  mutate(across(where(is.character), ~ str_replace_all(., regex("Centro de Salud Familiar", ignore_case = TRUE), "CESFAM")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Cerrillos De Nos", ignore_case = TRUE), "Ribera del Maipo")),
-         across(where(is.character), ~ str_replace_all(., regex("Posta de Salud Rural", ignore_case = TRUE), "PSR")),
-         across(where(is.character), ~ str_replace_all(., regex("Cecosf", ignore_case = TRUE), "CECOSF"))
-  )
+  ungroup()
 
 #rm(piv2024)
-#rm(piv2025)
-
+#rm(piv2025) # Si se descomentan estas líneas, se reduce el uso de memoria.
 
 ####################### Procesar base de datos usuarios Telesalud #####################
 
@@ -96,91 +89,93 @@ servicios_salud_validos <- c(
   "Municipalidad de San Miguel"
 )
 
-# 4. Filtrar base
+# 4. Filtrar base y limpiar nombres
 usuarios <- usuarios %>%
   filter(!run %in% runs_a_eliminar) %>%
-  filter(servicio_salud %in% servicios_salud_validos) %>% 
-  mutate(across(where(is.character), ~ str_replace_all(., regex("Centro de Salud Familiar", ignore_case = TRUE), "CESFAM")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Centro Comunitario de Salud Familiar", ignore_case = TRUE), "CECOSF")),
-         across(where(is.character), ~ str_replace_all(., regex("Cerrillos De Nos", ignore_case = TRUE), "Ribera del Maipo")),
-         across(where(is.character), ~ str_replace_all(., regex("Posta de Salud Rural", ignore_case = TRUE), "PSR")),
-         across(where(is.character), ~ str_replace_all(., regex("Cecosf", ignore_case = TRUE), "CECOSF"))
-  )
+  filter(servicio_salud %in% servicios_salud_validos) %>%
+  limpiar_nombres_centro()
 
 unique(usuarios$organizacion)
 unique(piv_estab_2024$centro)
+
 ####################### Manejo de la base de telesalud #####################
 
-last_sol_date <- max(as.Date(data$fecha_solicitud, format='%d-%m-%Y %H:%M'))
+# La fecha de corte se calcula con el 'data' original, pero la fecha debe ser POSIXct primero
+data$fecha_solicitud_posix <- ymd_hms(data$fecha_solicitud, tz = "UTC")
+
+last_sol_date <- max(data$fecha_solicitud_posix, na.rm = TRUE)
 first_day_this_month <- floor_date(last_sol_date, unit = "month")
 last_day_previous_month <- rollback(first_day_this_month)
 
 data1 <- data %>%
-  select(id,cesfam,prioridad,fecha_solicitud,direccion,
-         tipo_prestador,motivo_consulta,especificidad,
+  select(id, cesfam, prioridad, fecha_solicitud, direccion,
+         tipo_prestador, motivo_consulta, especificidad,
          estado, fecha_cierre, tipo_cierre, cargo,
          profesion, fecha_agenda, fecha_derivacion_1) %>%
-  rename(centro = cesfam) %>% 
-    mutate(
-    ##Gestiono las fechas##
-    fecha_solicitud = ymd_hms(fecha_solicitud, tz = "UTC"),
+  rename(centro = cesfam) %>%
+  mutate(
+    ## Gestiono las fechas (uso de floor_date) ##
+    fecha_solicitud = ymd_hms(fecha_solicitud, tz = "UTC"), # Asegurar POSIXct
     hora_sol = hour(fecha_solicitud),
-    fecha_solicitud = as.Date(fecha_solicitud, format='%d-%m-%Y %H:%M'),
-    fecha_cierre = as.Date(fecha_cierre, format='%d-%m-%Y %H:%M'),
-    fecha_agenda  = as.Date(fecha_agenda, format='%d-%m-%Y %H:%M'),
-    fecha_derivacion_1  = as.Date(fecha_derivacion_1, format='%d-%m-%Y %H:%M'),
+    fecha_cierre = ymd_hms(fecha_cierre, tz = "UTC"),
+    fecha_agenda = ymd_hms(fecha_agenda, tz = "UTC"),
+    fecha_derivacion_1 = ymd_hms(fecha_derivacion_1, tz = "UTC"),
+    
+    # Se convierte a Date sin formato redundante
+    fecha_solicitud = as.Date(fecha_solicitud),
+    fecha_cierre = as.Date(fecha_cierre),
+    fecha_agenda = as.Date(fecha_agenda),
+    fecha_derivacion_1 = as.Date(fecha_derivacion_1),
+    
     mes_sol = month(fecha_solicitud),
     ano_sol = year(fecha_solicitud),
-    dia_sol = weekdays(fecha_solicitud),
+    dia_sol = weekdays(fecha_solicitud, abbreviate = FALSE), # Mejor especificar
     mes_cierre = month(fecha_cierre),
     ano_cierre = year(fecha_cierre),
-    month_agenda = month(fecha_agenda),
-    year_agenda = year(fecha_agenda),
-    mes_deriv = month(fecha_derivacion_1),
-    year_deriv = year(fecha_derivacion_1),
-    month_year_sol = ymd(paste(ano_sol, mes_sol, "1", sep = "-")),
-    month_year_cierre = ymd(paste(ano_cierre, mes_cierre, "1", sep = "-")), 
-    month_year_agenda = ymd(paste(year_agenda, month_agenda, "1", sep = "-")),
-    month_year_deriv = ymd(paste(year_deriv, mes_deriv, "1", sep = "-")),
-    ##Cambio los nombres para join con base deis##
-    centro = ifelse(centro == 'CESFAM Eduardo Frei Montalva',
-                    'Centro de Salud Familiar Eduardo Frei Montalva', centro),
-    centro = ifelse(centro == 'Centro Comunitario de Salud Familiar Juan Aravena',
-                    'Centro Comunitario de Salud Familiar Dr. Salvador Allende', centro),
-    centro = ifelse(centro == 'Centro de Salud Familiar Juan Pablo II',
-                    'Centro de Salud Familiar Juan Pablo II ( San Bernardo)', centro),
-    centro = ifelse(centro == 'CECOSF Juan Aravena',
-                    'Centro Comunitario de Salud Familiar Dr. Salvador Allende', centro),
-    centro = ifelse(centro == 'CECOSF Eduardo Frei Montalva',
-                    'Centro Comunitario de Salud Familiar Eduardo Frei Montalva', centro),
-    centro = ifelse(centro == 'Centro Comunitario De Salud Familiar Cerrillos De Nos',
-                    'Centro Comunitario de Salud Familiar Ribera del Maipo', centro),
-    centro = ifelse(centro == 'CECOSF Dr. Miguel Enríquez Espinosa',
-                    'Centro Comunitario de Salud Familiar Dr. Miguel Enríquez Espinosa', centro)) %>%
+    
+    # Uso floor_date para generar las columnas month_year
+    month_year_sol = floor_date(fecha_solicitud, "month"),
+    month_year_cierre = floor_date(fecha_cierre, "month"),
+    month_year_agenda = floor_date(fecha_agenda, "month"),
+    month_year_deriv = floor_date(fecha_derivacion_1, "month"),
+    
+    ## Cambio los nombres para join con base deis (uso de case_when) ##
+    centro = case_when(
+      centro == 'CESFAM Eduardo Frei Montalva' ~ 'Centro de Salud Familiar Eduardo Frei Montalva',
+      centro == 'Centro Comunitario de Salud Familiar Juan Aravena' ~ 'Centro Comunitario de Salud Familiar Dr. Salvador Allende',
+      centro == 'Centro de Salud Familiar Juan Pablo II' ~ 'Centro de Salud Familiar Juan Pablo II ( San Bernardo)',
+      centro == 'CECOSF Juan Aravena' ~ 'Centro Comunitario de Salud Familiar Dr. Salvador Allende',
+      centro == 'CECOSF Eduardo Frei Montalva' ~ 'Centro Comunitario de Salud Familiar Eduardo Frei Montalva',
+      centro == 'Centro Comunitario De Salud Familiar Cerrillos De Nos' ~ 'Centro Comunitario de Salud Familiar Ribera del Maipo',
+      centro == 'CECOSF Dr. Miguel Enríquez Espinosa' ~ 'Centro Comunitario de Salud Familiar Dr. Miguel Enríquez Espinosa',
+      TRUE ~ centro # Mantener el valor original si no coincide
+    )
+  ) %>%
+  # El filtrado al mes anterior se aplica correctamente
   filter(fecha_solicitud < first_day_this_month) %>%
-  left_join(deis %>% select(codigo_centro, comuna, centro)) %>%
+  # El join es CRÍTICO: depende de las correcciones manuales de 'centro'
+  left_join(deis %>% select(codigo_centro, comuna, centro), by = "centro") %>%
   select(
     id, comuna, codigo_centro, centro, prioridad, fecha_solicitud,
     tipo_prestador, motivo_consulta, especificidad, estado,
     fecha_cierre, tipo_cierre, cargo, profesion,
     fecha_agenda, fecha_derivacion_1, month_year_sol, month_year_cierre, month_year_agenda,
-    month_year_deriv, dia_sol, hora_sol)
+    month_year_deriv, dia_sol, hora_sol
+  )
 
 
-
-#rm(data)
+#rm(data) # Se puede comentar o descomentar según necesidad de memoria
 rm(deis)
-
 
 ################### Dataframe por comuna ########################################
 
-data_por_comuna <-  split(data1, data1$comuna)
+# La variable 'data_por_comuna' es una lista filtrada por comuna.
+data_por_comuna <- split(data1, data1$comuna)
 
 ################### Dataframe por establecimiento ########################################
 
-
-data_por_establecimiento <-  split(data1, data1$centro)
+# La variable 'data_por_establecimiento' es una lista filtrada por establecimiento.
+data_por_establecimiento <- split(data1, data1$centro)
 
 #cesfam_piv <- piv_estab %>%
 #  rename(`n` = piv_2024
